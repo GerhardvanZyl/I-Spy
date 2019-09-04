38cbf2dfdf4271109621ed44144ce3954624912c
Gerhard van Zyl <gerhardvz@k2.com>
2019-09-04 21:18:55 +0200

    UI changes

--- wwwroot/script/app.js
+++ wwwroot/script/app.js
@@ -1,39 +1,54 @@
 (function () {
     $("#upload").on("change", () => {
-        $("#status-container").html("Uploading");
-        $("#description").html("");
-
         let formData = new FormData();
         let file = $("#upload")[0].files[0];
+
+        if (!file) return;
+
+        $(".lds-circle")[0].style.display = "inline-block";
+        $("#image-container").hide();
+        $("#description").html("...");
+
+
         formData.append("files", file);
 
         $.ajax({
             url: "api/image",
             type: "POST",
             data: formData,
-            //dataType: "json",
             processData: false,
             contentType: false
         })
             .done((resultRaw) => {
-                console.log("result: ", resultRaw);
-                let result = JSON.parse(resultRaw);
-
-                $("#status-container").html("");
-
                 var reader = new FileReader();
                 reader.onload = function (e) {
-                    $("#uploaded-image").attr("src", e.target.result)
+                    $("#uploaded-image").attr("src", e.target.result);
                 }
 
                 reader.readAsDataURL(file);
+                
+                console.log("result: ", resultRaw);
+                let result = JSON.parse(resultRaw);
+                let tags = "";
 
                 if (result.message) {
-                    $("#status-container").html(result.message + " - Try a different image");
+                    $("#description").html(result.message + " - Try a different image");
                 } else {
                     let description = result.description.captions[0].text;
-                    $("#description").html(description);
+                    // let firstLetter = description.substr(0,1).toLocaleUpperCase();
+                    // let sentenceDescription = firstLetter + description.substr(1);
+                    $("#description").html("I see " + description + ".");
+
+                    for (tag of result.description.tags){
+                        tags += `<span>#${tag}</span> `;
+                    }
+
                 }
+
+                $(".lds-circle")[0].style.display = "none";
+                $("#image-container").show();
+                $("#tags").html(tags);
+
             });
 
     });
